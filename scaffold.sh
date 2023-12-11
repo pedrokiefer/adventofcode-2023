e249be06455a1d02066daf64b569a13e07d17ee8
Pedro Kiefer <pedro@kiefer.com.br>
2023-12-10 21:40:30 -0300

    chore: improve scaffold

--- scaffold.sh
+++ scaffold.sh
@@ -14,4 +14,4 @@ dirName="day$1"
 mkdir -p $dirName
 
 echo -e "package main\n\nfunc main() {\n\n}\n" > "$dirName/main.go"
-echo -e "package main\n\nimport \"testing\"\n\nfunc Test(t *testing.T) {\n\n}\n" > "$dirName/${dirName}_test.go"
+echo -e "package main\n\nimport (\n\t\"bytes\"\n\t\"io\"\n\t\"testing\"\n)\n\nfunc Test(t *testing.T) {\ninput := io.NopCloser(bytes.NewReader([]byte(``)))\n\n}\n" > "$dirName/${dirName}_test.go"
